8765157e62044f4058171cbe1f6d36672930ba9d
feluisdev <feluis.dev@gmail.com>
2025-08-26 20:54:58 -0100

    intial commit

---
+++ src/main/java/cv/igrp/license/LicenseConfigApplication.java
@@ -0,0 +1,30 @@
+package cv.igrp.license;
+
+import cv.igrp.license.shared.config.ApplicationAuditorAware;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAware", dateTimeProviderRef = "auditDateTimeProvider")
+public class LicenseConfigApplication {
+
+  @Bean
+  public AuditorAware<String> auditAware() {
+    return new ApplicationAuditorAware();
+  }
+
+  @Bean
+  public DateTimeProvider auditDateTimeProvider() {
+    return () -> Optional.of(LocalDateTime.now());
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(LicenseConfigApplication.class, args);
+  }
+}
